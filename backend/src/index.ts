a423ca9c23824840d9d60a1f5609f5f98c9cc6f2
Kelvin Hemu <kelvinhemu@gmail.com>
2025-04-26 17:47:22 +0300

    changes ss

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
-import app from "./app.js";
+import app from "./app";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-console.log(Server running on port ${PORT});
+  console.log(`Server running on port ${PORT}`);
+}).on('error', (err) => {
+  console.error('Server error:', err);
 });
